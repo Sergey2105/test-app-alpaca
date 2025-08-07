db3e998b45f708c8bbc28a2a94e054e23353e17a
Sergey Roschin <Sergey210500@gmail.com>
2025-08-07 12:49:19 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ export default defineConfig({
   server: {
     host: true,
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
+    },
+  },
 });
