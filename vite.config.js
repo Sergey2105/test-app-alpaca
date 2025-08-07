a4027c4f2877a23ba6bca6363ec1542f2369030e
Sergey Roschin <Sergey210500@gmail.com>
2025-08-07 10:59:07 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  css: {
-    preprocessorOptions: {
-      scss: {},
-    },
-  },
   base: "/test-app-alpaca/",
   server: {
     host: true,
